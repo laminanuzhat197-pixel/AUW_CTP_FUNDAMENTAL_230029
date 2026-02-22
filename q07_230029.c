0b0ee667a84d8d5f685d3353520ea002e77a4364
laminanuzhat197-pixel <laminanuzhat197@gmail.com>
2026-02-22 21:19:57 +0600

    Add files via upload

---
+++ q07_230029.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 5;
+    float f = 3.5;
+    char c = 'Z';
+
+    printf("%d %.2f %c\n", x, f, c);
+}
---
+++ q09_230029.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float f;
+    scanf("%f", &f);
+    printf("%.2f\n", f);
+}
